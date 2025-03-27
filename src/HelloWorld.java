cac8c31723a8114b4c410ea9b365947bea057cf3
phspopos <phspopos@naver.com>
2025-03-27 17:54:48 +0900

    커밋 되나

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
 		System.out.println("이클립스 첫 실행");
 		System.out.println("Hello Eclipse");
 		System.out.println("Github연동완료");
-		
+		System.out.println("연동 2번째");
 	}
 
 }
